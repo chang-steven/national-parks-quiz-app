7c824c1d97fea6995d08323633ed63692af67ccf
Chang-Steven <steven.chang.dev@gmail.com>
2017-09-01 13:35:56 -0700

    edited .js file and cleaned up syntax

--- index.v3.js
+++ index.v3.js
@@ -1,13 +1,11 @@
 //Sets the global object 'currentState' which will be available to the function set.  'currentState' will be populated by the function 'setCurrentState'
 //'currentSet' to the current iteration of the question and answer set from 'quizSet'
-
 let currentState = {
   questionNumber: 0,
   userCorrect: 0,
   userWrong: 0,
   totalQuestions: null
   } ;
-
 
 //On page load 'quizApp' will run.  This function will perform the following:
 //1. call the function 'startQuizPrompt'
@@ -16,27 +14,14 @@ let currentState = {
 //4. call 'listenForClick'
 //5. call 'renderQuestionPage'
 
-$(quizApp);
-
-function quizApp() {
-  console.log('`quizApp` ran');
-  startQuizPrompt();
-  currentState.totalQuestions = quizSet.length;
-  setCurrentState(quizSet[currentState.questionNumber]);
-  listenForClick();
-  renderQuestionPage();
-};
-
 //This function sets the state of 'currentState' which is the current question and answer data from 'quizSet'
-
 function setCurrentState (quizInput) {
   for (var key in quizInput) {
     currentState[key] = quizInput[key]
-  }
-};
+  };
+}
 
 //This function will update the question page with data from 'currentState' and populate the HTML containers with the corresponding quiz text
-
 function renderQuestionPage() {
   console.log('`renderQuestionPage` ran');
   clearForm();
@@ -48,7 +33,7 @@ function renderQuestionPage() {
   $('#js-ans4').text(currentState.choices[3]);
   $('.js-current-question').text('Question ' + (currentState.questionNumber + 1) + ' out of ' + currentState.totalQuestions);
   $('.js-current-score').text('Score: ' + currentState.userCorrect + ' Correct & ' + currentState.userWrong + ' Incorrect');
-};
+}
 
 //This function will initiate all event listeners for clicks and submit.
 //'submit' on submit will grab the value of the form and compare with the correct answer from 'currentState'.  If correct, will pass 'true' to 'showFeedbackPage' and increment the value for 'userCorrect' and if Incorrect will pass 'false' to 'showFeedbackPage' and increment the value for 'userWrong'.
@@ -78,11 +63,9 @@ function listenForClick() {
     $('.js-next').click(currentState, clickNext);
     $('.js-previous').click(currentState, clickPrevious);
     $('.js-startover').click(currentState, startOver);
-};
-
+}
 
 //This is the callback function which will call 'showResultspage' if end of the quiz or increment the key 'questionNumber' and pass currentState to 'showResultspage'.  Otherwise will increment 'questionNumber' and will re-run 'setCurrentState' and 'renderQuestionPage'
-
   function clickNext() {
     console.log('`clickNext` ran');
     if (currentState.questionNumber+1 == currentState.totalQuestions) {
@@ -139,7 +122,7 @@ function listenForClick() {
       };
 
 //This code will populate the blurb and image data and post to the user
-      $('.js-answer-blurb').text( currentState.blurb );
+      $('.js-answer-blurb').text(currentState.blurb);
       $('#feedback-page').find('img').attr('src', currentState.image);
 
       if (boolean == true) {
@@ -156,37 +139,29 @@ function listenForClick() {
     console.log('`showResultspage` ran');
     let modal = document.getElementById('results-page');
     let exit = document.getElementsByClassName("close")[1];
-
       modal.style.display = "block";
-
       exit.onclick = function() {
         modal.style.display = "none";
       };
-
       $('.js-next').click( function() {
         modal.style.display = "none";
       });
-
       $('.js-startover').click( function() {
         modal.style.display = "none";
       });
-
       window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
       };
-
     $('.js-final-score').text( 'Your final score: ' + currentState.userCorrect + ' out of 10' );
     if (currentState.userCorrect > 6) {
       $('.js-results-header').text("Congratulations, you're a Junior Park Ranger!");
       }
-
     else {
       $('.js-results-header').text("Sorry, looks like you need to visit more national parks.");
       }
-  };
-
+  }
 
 //This function resets radio inputs
   function clearForm() {
@@ -195,23 +170,18 @@ function listenForClick() {
     console.log('`clearForm` ran');
   }
 
-
 //On page load function start quiz will load the start page prompt
 function startQuizPrompt() {
   let modal = document.getElementById('start-page');
   let exit = document.getElementsByClassName("close")[0];
-
     modal.style.display = "block";
     console.log('`startQuizPrompt` ran');
-
   $('.js-take-quiz-button').click( function() {
     modal.style.display = "none";
     });
-
   exit.onclick = function() {
     modal.style.display = "none";
     };
-
   window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -227,35 +197,16 @@ function startOver () {
     currentState.questionNumber = 0;
     setCurrentState(quizSet[currentState.questionNumber]);
     renderQuestionPage();
-  }
+  };
 }
 
+function quizApp() {
+  console.log('`quizApp` ran');
+  startQuizPrompt();
+  currentState.totalQuestions = quizSet.length;
+  setCurrentState(quizSet[currentState.questionNumber]);
+  listenForClick();
+  renderQuestionPage();
+}
 
-
-
-
-
-// function showPopup() {
-//   let modal = document.getElementById('popupBox');
-//   let exit = document.getElementsByClassName("close")[0];
-//
-//   // window.onload = function() {
-//     modal.style.display = "block";
-//     // $('#start-page').toggleClass('hidden')
-//     console.log('`showPopup` ran');
-//   // };
-//
-//   $('.js-modal-click').click( function() {
-//     modal.style.display = "none";
-//   });
-//
-//   exit.onclick = function() {
-//     modal.style.display = "none";
-//   };
-//
-//   window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-//   }
-// };
+$(quizApp);
